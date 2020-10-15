37191ee86583147e1d4bb67525222cdacbb83866
seanmcgeehan <seanmcgeehan@localhost>
2020-10-15 13:15:15 -0400

    getters and setters

--- gitfun/src/gitfun/itemToPurchase.java
+++ gitfun/src/gitfun/itemToPurchase.java
@@ -2,5 +2,26 @@ package gitfun;
 
 public class itemToPurchase {
 	String name;
-
+	int price;
+	int amount;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
 }
